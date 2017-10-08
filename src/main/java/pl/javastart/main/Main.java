d4258c66a848f4958c3c82b8e4d1965c0721d7c1
Daniel Gutowski <gutowiaczek@gmail.com>
2017-10-08 16:23:21 +0200

    inicjalna wersja projektu

---
+++ src/main/java/pl/javastart/main/Main.java
@@ -0,0 +1,15 @@
+package pl.javastart.main;
+
+import pl.javastart.dao.BookDao;
+import pl.javastart.dao.BookDaoImpl;
+import pl.javastart.model.Book;
+
+public class Main{
+    public static void main(String[] args) {
+        Book book = new Book("12345678912345678","TITLE","JANEK");
+        BookDao bookDao = new BookDaoImpl();
+        bookDao.save(book);
+        System.out.println("Saved books");
+        bookDao.cleanUp();
+    }
+}
---
+++ src/main/java/pl/javastart/model/Book.java
@@ -0,0 +1,75 @@
+package pl.javastart.model;
+
+import java.io.Serializable;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "BOOK")
+public class Book implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "isbn", nullable = false)
+    private String isbn;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", isbn="
+                + isbn + ", title=" + title + ", author=" + author + "]";
+    }
+}
